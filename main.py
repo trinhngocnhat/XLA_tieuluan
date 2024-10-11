bfa7ae1224096ac307ab30e92005c30beb22659e
Trịnh Ngọc Nhất <trinhngocnhat079@gmail.com>
2024-10-11 19:53:11 +0700

    trinhngocnhat_10/11/2024

--- main.py
+++ main.py
@@ -1,50 +1,59 @@
-import cv2
-import pytesseract
-import numpy as np
-import imutils
-from imutils.contours import sort_contours
+# Loading the required python modules
+import pytesseract # this is tesseract module
+import matplotlib.pyplot as plt
+import cv2 # this is opencv module
+import glob
 import os
-from sklearn.model_selection import train_test_split
-from sklearn.svm import SVC
-import joblib
 
-pytesseract.pytesseract.tesseract_cmd = r'C:\Program Files\Tesseract-OCR\tesseract.exe'
+# specify path to the license plate images folder as shown below
+path_for_license_plates = os.getcwd() + "/license-plates/**/*.jpg"
+list_license_plates = []
+predicted_license_plates = []
 
-def detect_license_plate(image):
-    gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
-    # Apply bilateral filter to reduce noise while keeping edges sharp
-    gray = cv2.bilateralFilter(gray, 11, 17, 17)
-    # Edge detection
-    edged = cv2.Canny(gray, 30, 200)
+for path_to_license_plate in glob.glob(path_for_license_plates, recursive=True):
+    license_plate_file = path_to_license_plate.split("/")[-1]
+    license_plate, _ = os.path.splitext(license_plate_file)
+    ''' 
+    Here we append the actual license plate to a list 
+    '''
+    list_license_plates.append(license_plate)
 
-    # Find contours based on edges detected
-    cnts, _ = cv2.findContours(edged.copy(), cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
-    cnts = sorted(cnts, key=cv2.contourArea, reverse=True)[:10]
-    license_plate = None
+    ''' 
+    Read each license plate image file using openCV 
+    '''
+    img = cv2.imread(path_to_license_plate)
 
-    for c in cnts:
-        # Approximate the contour
-        peri = cv2.arcLength(c, True)
-        approx = cv2.approxPolyDP(c, 0.018 * peri, True)
+    ''' 
+    We then pass each license plate image file 
+    to the Tesseract OCR engine using the Python library 
+    wrapper for it. We get back predicted_result for 
+    license plate. We append the predicted_result in a 
+    list and compare it with the original the license plate 
+    '''
+    predicted_result = pytesseract.image_to_string(img, lang='eng',
+                                                   config='--oem 3 --psm 6 -c tessedit_char_whitelist = ABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789')
 
-        # The license plate should be a rectangle (4 sides)
-        if len(approx) == 4:
-            x, y, w, h = cv2.boundingRect(c)
-            license_plate = image[y:y + h, x:x + w]
-            return license_plate
+    filter_predicted_result = "".join(predicted_result.split()).replace(":", "").replace("-", "")
+    predicted_license_plates.append(filter_predicted_result)
 
-    return None
+print("Actual License Plate", "\t", "Predicted License Plate", "\t", "Accuracy")
+print("--------------------", "\t", "-----------------------", "\t", "--------")
 
-def preprocess_plate(plate_img):
-    gray = cv2.cvtColor(plate_img, cv2.COLOR_BGR2GRAY)
-    # Apply threshold to get binary image
-    _, thresh = cv2.threshold(gray, 150, 255, cv2.THRESH_BINARY)
-    return thresh
 
-def recognize_text(preprocessed_img):
-    config = '-l eng --oem 1 --psm 7'  # OEM and PSM settings can be adjusted
-    text = pytesseract.image_to_string(preprocessed_img, config=config)
-    # Clean the text
-    text = ''.join(e for e in text if e.isalnum())
-    return text
+def calculate_predicted_accuracy(actual_list, predicted_list):
+    for actual_plate, predict_plate in zip(actual_list, predicted_list):
+        accuracy = "0 %"
+        num_matches = 0
+        if actual_plate == predict_plate:
+            accuracy = "100 %"
+        else:
+            if len(actual_plate) == len(predict_plate):
+                for a, p in zip(actual_plate, predict_plate):
+                    if a == p:
+                        num_matches += 1
+                accuracy = str(round((num_matches / len(actual_plate)), 2) * 100)
+                accuracy += "%"
+        print("	 ", actual_plate, "\t\t\t", predict_plate, "\t\t ", accuracy)
 
+
+calculate_predicted_accuracy(list_license_plates, predicted_license_plates)
