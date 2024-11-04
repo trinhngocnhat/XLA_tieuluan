a023ccdd2e8c239bb31e81007f22d32a348764c1
Trịnh Ngọc Nhất <trinhngocnhat079@gmail.com>
2024-11-04 22:58:20 +0700

    Update testGUI.py

--- testGUI.py
+++ testGUI.py
@@ -3,24 +3,14 @@ from chuc_nang.chuc_nang_chinh.chon_hinh_anh import chon_hinh_anh
 from chuc_nang.chuc_nang_chinh.open_camera import livestream
 
 root = tk.Tk()
-root.title("Nhận diện biển số xe ")
+root.title("Nhận diện biển số xe")
+root.geometry("1024x632")
 
-#Set window size
-root.geometry("400x300")
+root.grid_rowconfigure([0, 1, 2], weight=1)
+root.grid_columnconfigure([0, 1], weight=1)
 
-label = tk.Label(root, text="Hãy chọn chức năng bạn muốn ")
-label.pack()
-
-# Configure rows and columns to center all elements
-root.grid_rowconfigure(0, weight=1)  # Top space
-root.grid_rowconfigure(1, weight=1)  # Centered content
-root.grid_rowconfigure(2, weight=1)  # Bottom space
-root.grid_columnconfigure(0, weight=1)  # Left space
-root.grid_columnconfigure(1, weight=1)  # Right space
-
-# Use grid for label and buttons
-label.grid(row=0, column=0, columnspan=5, pady=20)
-tk.Button(root, text="livestream", command=livestream ).grid(row=1, column=0, padx=10, pady=10)
+tk.Label(root, text="Hãy chọn chức năng bạn muốn").grid(row=0, column=0, columnspan=5, pady=20)
+tk.Button(root, text="livestream", command=livestream).grid(row=1, column=0, padx=10, pady=10)
 tk.Button(root, text="chọn ảnh", command=chon_hinh_anh).grid(row=1, column=1, padx=10, pady=10)
 
 root.mainloop()
